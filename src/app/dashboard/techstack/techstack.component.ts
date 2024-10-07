edc11e8de17347c036ab9a9e20b339b602ed7543
subhasiskhuntia <subhasiskhuntia506@gmail.com>
2024-10-08 00:12:02 +0530

    some minor changes

--- src/app/dashboard/techstack/techstack.component.ts
+++ src/app/dashboard/techstack/techstack.component.ts
@@ -14,51 +14,51 @@ export class TechstackComponent implements OnInit {
     if(this.type=="Uses"){
       this.list=[
         {
-          image:"/asset/vscode3-svgrepo-com.svg",
+          image:"asset/vscode3-svgrepo-com.svg",
           title:"VS Code"
         },
         {
-          image:"/asset/intellij-idea-svgrepo-com.svg",
+          image:"asset/intellij-idea-svgrepo-com.svg",
           title:"Intellij"
         },
         {
-          image:"/asset/jira-svgrepo-com.svg",
+          image:"asset/jira-svgrepo-com.svg",
           title:"Jira"
         },
         {
-          image:"/asset/bitbucket-svgrepo-com.svg",
+          image:"asset/bitbucket-svgrepo-com.svg",
           title:"Bitbucket"
         },
         {
-          image:"/asset/excel-svgrepo-com.svg",
+          image:"asset/excel-svgrepo-com.svg",
           title:"Excel"
         },
         {
-          image:"/asset/jupyter-svgrepo-com.svg",
+          image:"asset/jupyter-svgrepo-com.svg",
           title:"Jupyter Notebook"
         },
         {
-          image:"/asset/postman-icon-svgrepo-com.svg",
+          image:"asset/postman-icon-svgrepo-com.svg",
           title:"PostMan"
         },
         {
-          image:"/asset/bootstrap-svgrepo-com.svg",
+          image:"asset/bootstrap-svgrepo-com.svg",
           title:"Bootstrap"
         },
         {
-          image:"/asset/eclipse-icon-svgrepo-com.svg",
+          image:"asset/eclipse-icon-svgrepo-com.svg",
           title:"Eclipse"
         },
         {
-          image:"/asset/git-svgrepo-com.svg",
+          image:"asset/git-svgrepo-com.svg",
           title:"Git"
         },
         {
-          image:"/asset/jfrog-svgrepo-com.svg",
+          image:"asset/jfrog-svgrepo-com.svg",
           title:"JFrog"
         },
         {
-          image:"/asset/maven-svgrepo-com.svg",
+          image:"asset/maven-svgrepo-com.svg",
           title:"Maven"
         },
       ]
@@ -66,67 +66,67 @@ export class TechstackComponent implements OnInit {
   }
   list:{image:string,title:string}[]=[
     {
-      image:"/asset/typescript-icon-svgrepo-com.svg",
+      image:"asset/typescript-icon-svgrepo-com.svg",
       title:"Typescript"
     },
     {
-      image:"/asset/download.png",
+      image:"asset/download.png",
       title:"Spring Boot"
     },
     {
-      image:"/asset/angular-icon.svg",
+      image:"asset/angular-icon.svg",
       title:"Angular"
     },
     {
-      image:"/asset/java-svgrepo-com.svg",
+      image:"asset/java-svgrepo-com.svg",
       title:"Java"
     },
     {
-      image:"/asset/aws-svgrepo-com.svg",
+      image:"asset/aws-svgrepo-com.svg",
       title:"AWS"
     },
     {
-      image:"/asset/docker-svgrepo-com.svg",
+      image:"asset/docker-svgrepo-com.svg",
       title:"Docker"
     },
     {
-      image:"/asset/oracle-svgrepo-com.svg",
+      image:"asset/oracle-svgrepo-com.svg",
       title:"Oracle DB"
     },
     {
-      image:"/asset/mongodb-svgrepo-com.svg",
+      image:"asset/mongodb-svgrepo-com.svg",
       title:"MongoDB"
     },
     {
-      image:"/asset/kafka-icon-svgrepo-com.svg",
+      image:"asset/kafka-icon-svgrepo-com.svg",
       title:"Kafka"
     },
     {
-      image:"/asset/graphql-svgrepo-com.svg",
+      image:"asset/graphql-svgrepo-com.svg",
       title:"GraphQL"
     },
     {
-      image:"/asset/jenkins-svgrepo-com.svg",
+      image:"asset/jenkins-svgrepo-com.svg",
       title:"Jenkins"
     },
     {
-      image:"/asset/openshift-svgrepo-com.svg",
+      image:"asset/openshift-svgrepo-com.svg",
       title:"Openshift"
     },
     {
-      image:"/asset/python-svgrepo-com.svg",
+      image:"asset/python-svgrepo-com.svg",
       title:"Python"
     },
     {
-      image:"/asset/react-javascript-js-framework-facebook-svgrepo-com.svg",
+      image:"asset/react-javascript-js-framework-facebook-svgrepo-com.svg",
       title:"React"
     },
     {
-      image:"/asset/js-svgrepo-com.svg",
+      image:"asset/js-svgrepo-com.svg",
       title:"Javascript"
     },
     {
-      image:"/asset/redis-logo-svgrepo-com.svg",
+      image:"asset/redis-logo-svgrepo-com.svg",
       title:"Redis"
     },
   ]
